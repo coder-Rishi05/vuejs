36e3e5ed6e90b68a3353a79e35e1e062eb1b2a57
coder-Rishi05 <rawatrishi1121@gmail.com>
2026-01-25 23:54:20 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+   <h1>Hello view js </h1>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+
+}
+
+h1{
+  color: antiquewhite;
+}
+</style>
